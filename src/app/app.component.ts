a1d3b10fee6c6234f7d3c6ef70c4d2598c4ef489
yurikb <yuri.kollerborba@gmail.com>
2022-04-19 15:05:12 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'task-tracker';
-}
+export class AppComponent {}
